f798a44b1822e818562b050600d1d8665d7658ee
Aaron Marks <Aaron.Marks@cdpehs.com>
2018-08-03 15:56:41 -0400

    Updated for story 70 (AddUpdateWicVendor).

---
+++ src/WUMEI/Models/2018.1-Preview/VendorPhysicalAddress.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WUMEI.Models._2018._1_Preview
+{
+    public class VendorPhysicalAddress
+    {
+        #region Properties
+
+        /// <summary>
+        /// Sub-element of address data elements identifying the first line of the address specified.
+        /// </summary>
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(CustomRegex.AbcNumSpecSpaceNameAndAddress)]
+        public string AddressLine1 { get; set; }
+
+        /// <summary>
+        /// Sub-element of address data elements identifying the second line, if present, of the address specified.
+        /// </summary>
+        [StringLength(30)]
+        [RegularExpression(CustomRegex.AbcNumSpecSpaceNameAndAddress)]
+        public string AddressLine2 { get; set; }
+
+        /// <summary>
+        /// Sub-element of address data elements identifying the city name of the address specified.
+        /// </summary>
+        [Required]
+        [StringLength(25)]
+        [RegularExpression(CustomRegex.AbcNumSpecSpaceNameAndAddress)]
+        public string AddressCity { get; set; }
+
+        /// <summary>
+        /// Sub-element of address data elements; conforms to the U.S. Post Office state and territory abbreviations.
+        /// </summary>
+        [Required]
+        [StringLength(2)]
+        [RegularExpression(CustomRegex.AbcNumSpecSpaceNameAndAddress)]
+        public string AddressState { get; set; }
+
+        /// <summary>
+        /// Sub-element of address data elements and a constructed data element including the Address ZIP code,
+        /// primary and the Address ZIP code, extended for the address specified.
+        /// </summary>
+        [Required]
+        public VendorPhysicalZipCode AddressZipCode { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/WUMEI/Models/2018.1-Preview/VendorPhysicalZipCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WUMEI.Models._2018._1_Preview
+{
+    public class VendorPhysicalZipCode
+    {
+        #region Properties
+
+        /// <summary>
+        /// A sub-element of Address ZIP code identifying the U.S. postal code associated with the address specified.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(5)]
+        [RegularExpression(CustomRegex.Num)]
+        public string ZipCodePrimary { get; set; }
+
+        /// <summary>
+        /// A sub-element of Address ZIP code, identifying the ZIP+4 U.S. postal codes for the address specified.
+        /// </summary>
+        [StringLength(4)]
+        [RegularExpression(CustomRegex.Num)]
+        public string ZipCodeExtended { get; set; }
+
+        #endregion
+    }
+}
